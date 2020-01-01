e166bd03c4e43682a893c4adf5e6a5aff876cdfe
Milkbio <1032635701@qq.com>
2020-01-01 20:50:47 +0800

    init 小程序

---
+++ pages/posts/posts.js
@@ -0,0 +1,76 @@
+// pages/posts/posts.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+      indicatorDots: true,
+      list: []
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+      let list = [{
+          postAuthorUrl: '/images/avatar/avatar1.jpg',
+          date: "Sep 18 2016",
+          title: "正是虾肥蟹壮时",
+          postImg: "/images/posts/posts1.jpg",
+          content: "菊黄蟹正肥，品尝秋之味。徐志摩把“看初花的荻芦”和“到楼外楼吃蟹”并列为秋天来杭州不能错过的风雅之事，用林妹妹的话说就是“敖封嫩玉双双满”。",
+          viewNum: 112,
+          collectNum: 96,
+      }]
+      this.setData({list})
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    console.log('reach bottom')
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
